6d8a95183d3dfebcf81d00ee4c737b14310662ab
Alex <doquocanh482001@gmail.com>
2021-11-05 23:31:31 +0700

    6th commit

--- market/__init__.py
+++ market/__init__.py
@@ -9,5 +9,7 @@ app.config['SECRET_KEY'] = 'c1fa0aea11d5a7c139e4bfe9'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from market import routes
 
--- market/routes.py
+++ market/routes.py
@@ -3,16 +3,18 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route('/')
 @app.route('/home')
 def home_page():
+
     return render_template('home.html')
 
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -27,6 +29,8 @@ def register_page():
                            password=form.password1.data)
         db.session.add(user_create)
         db.session.commit()
+        login_user(user_create)
+        flash(f"Account created successfully! You are logged in as {user_create.username}", category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}: #if have errors
         for err in form.errors.values():
@@ -46,4 +50,11 @@ def login_page():
         else:
             flash(f'Username or Password incorrect! Please try again.', category='danger')
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("home_page"))
 
